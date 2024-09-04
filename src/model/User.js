a5cd330bc5fbb0c6516fa27a3cd4d62f71ae5615
Felipe <phellyype@gmail.com>
2024-09-04 17:57:09 -0300

    Fix: including the default Date.now() at createAt property.

--- src/model/User.js
+++ src/model/User.js
@@ -11,14 +11,14 @@ const UserSchema = new mongoose.Schema({
             number: {type: Number, required: true},
             complement: {type: String, required: false},
             zipcode: {type: String, required: true},
-            createdAt: {type: Date, required: true}
+            createdAt: {type: Date, required: true, default: Date.now()}
         }
     ],
-    createAt: {type: Date, required: true},
+    createAt: {type: Date, required: true, default: Date.now()},
     favorite_products: [
         {
             _id: {type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "products"},
-            createdAt: {type: Date, required: true}
+            createdAt: {type: Date, required: true, default: Date.now()}
         }
     ],
     adminUser: {type: Boolean, required: true, default: false}
