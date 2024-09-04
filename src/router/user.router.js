c443dbd12026b4466a126f0dae3b9624692868bd
Felipe <phellyype@gmail.com>
2024-09-04 11:21:21 -0300

    Docs: comments in file user.routes

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -2,15 +2,19 @@ const express = require("express")
 const router = express.Router()
 const userController = require("../controller/user.controller")
 
+// GET routes
 router.get("/findById/:id", userController.findUserById)
 router.get("/findAll", userController.findAllUsers)
 
+// POST routes
 router.post("/create", userController.createUser)
 router.post("/addAddress/:id", userController.addUserAddress)
 router.post("/addFavoriteProduct/:id", userController.addUserFavoriteProduct)
 
+// PUT route
 router.put("/update/:id", userController.updateUser)
 
+// DELETE routes
 router.delete("/remove/:id", userController.removeUser)
 router.delete("/removeAddress", userController.removeUserAddress)
 router.delete("/removeFavoriteProduct", userController.removeUserFavoriteProduct)
