e08482370f5aabfed86969190a30d5c130918271
Felipe <phellyype@gmail.com>
2024-09-04 17:54:24 -0300

    Fix: Include marketplace into the database config file.

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectToDatabase = () => {
     try{
-        mongoose.connect("mongodb://localhost:27017")
+        mongoose.connect("mongodb://localhost:27017/marketplace")
         console.log(`The MongoDB server is connected.`)
     }
     catch(err){
