dfc280c12d0c3ddcf97a1ba774b396212e57914c
Felipe <phellyype@gmail.com>
2024-09-04 10:51:30 -0300

    Feat: User model was created.

---
+++ src/model/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    image: {type: String},
+    address: [
+        {
+            street: {type: String, required: true},
+            number: {type: Number, required: true},
+            complement: {type: String, required: false},
+            zipcode: {type: String, required: true},
+            createdAt: {type: Date, required: true}
+        }
+    ],
+    createAt: {type: Date, required: true},
+    favorite_products: [
+        {
+            _id: {type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "products"},
+            createdAt: {type: Date, required: true}
+        }
+    ],
+    adminUser: {type: Boolean, required: true, default: false}
+})
+
+const User = mongoose.model("users", UserSchema)
+
+module.exports = User
